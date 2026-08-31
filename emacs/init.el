2804ae23e9f3aa778ef3b5e3e6775593e1aede94
Krishna Jani <krishnajani@macboi.local>
2026-08-31 20:00:52 +0530

    Emacs Bankruptcy

--- emacs/init.el
+++ emacs/init.el
@@ -1,185 +1,114 @@
-;; -*- lexical-binding: t; -*-
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
-(unless (package-installed-p 'use-package)
- (package-install 'use-package))
-
-
-;; Configs
-(load-file "~/.emacs.d/kj-org.el") ;; Org mode and text mode changes
-
-(setq ispell-dictionary "british")
-(setq send-mail-function 'smtpmail-send-it)
-(setq smtpmail-smtp-server "posteo.de")
-(setq smtpmail-smtp-service 587)
-(setq smtpmail-smtp-user "krishnajani@posteo.com")
-(setq smtpmail-servers-requiring-authorization "posteo.de")
-;(org-babel-load-file "scimax-editmarks.org")
-
-;(server-start)
-(require 'org-protocol)
-
-(setq package-install-upgrade-built-in t)
-
-(use-package consult
-  :ensure t
-  :bind
-  ("C-x b" . 'consult-buffer)
-  ("C-c s" . 'consult-line)
-  ("C-c f" . 'consult-find))
-
-(use-package vertico
-  :ensure t
-  :config
-  (vertico-mode 1))
-
-(use-package marginalia
-  :ensure t
-  :config
-  (marginalia-mode +1))
-
-(use-package magit
-  :ensure t
-  :bind
-  ("C-x g" . 'magit)
-  ) 
-
-(setq-default line-spacing 0.15)
-
-
-;; Generic UI Changes. Disables all GUI elements, such as toolbar, scrollbar and menubar
-
+;; UI Elements
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(recentf-mode)
-(visual-line-mode -1)
 
-;; Notmuch
+;;Package.el
+(require 'package)
 
-(use-package notmuch
-  :ensure t
-  :bind
-  ("C-c m" . 'notmuch))
+;; Add third-party package repositories
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/" ) t)
+(add-to-list 'package-archives '("nongnu" . "https://nongnu.org" ) t)
 
+;; Initialize the package system
+(package-initialize)
 
-(set-face-attribute 'mode-line nil :height 140) ;; Sets height to 14pt (140 tenths of a point)
-(set-face-attribute 'mode-line-inactive nil :height 140)
-
-(defun my-configure-font (frame)
-  "Configure font given initial non-daemon FRAME.
-Intended for `after-make-frame-functions'."
-  (set-face-attribute 'default nil
-		      :font "LiterationMono Nerd Font"
-		      :height 140)
-  (set-face-attribute 'variable-pitch nil
-		      :font "Verdana"
-		      :height 140)
-  (set-face-attribute 'fixed-pitch nil
-		      :font "PT Mono"
-		      :height 140))
-;  (remove-hook 'after-make-frame-functions #'my-configure-font))
-
-(add-hook 'after-make-frame-functions #'my-configure-font)
-;; Simple font configuration
-(set-face-attribute 'default nil
-		    :font "LiterationMono Nerd Font"
-		    :height 130)
-(set-face-attribute 'variable-pitch nil
-		    :font "Verdana"
-		    :height 140)
-(set-face-attribute 'fixed-pitch nil
-		    :font "PT Mono"
-		    :height 140)
-(global-set-key (kbd "C-c w") 'count-words)
+;; Fetch the package list from archives if not already done
+(unless package-archive-contents
+  (package-refresh-contents))
 
 
-(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
-      gc-cons-percentage 0.6)
-(defvar config:file-name-handler-alist-cache file-name-handler-alist)
-(setq file-name-handler-alist nil)
-(defun config:restore-post-init-settings ()
-  (setq gc-cons-threshold 16777216 ; 16mb
-        gc-cons-percentage 0.1)
-  (setq file-name-handler-alist config:file-name-handler-alist-cache))
-(add-hook 'emacs-startup-hook #'config:restore-post-init-settings)
+(add-to-list 'default-frame-alist '(background-color . "honeydew"))
 
-(defun config:defer-gc ()
-  (setq gc-cons-threshold most-positive-fixnum))
-(defun config:-do-restore-gc ()
-  (setq gc-cons-threshold 16777216))
-(defun config:restore-gc ()
-  (run-at-time 1 nil #'config:-do-restore-gc))
+;; Latex Keybinds
 
-(add-hook 'minibuffer-setup #'config:defer-gc)
-(add-hook 'minibuffer-exit #'config:restore-gc)
+;; Enable Abbrev Mode automatically when opening a LaTeX file
+(add-hook 'latex-mode-hook 'abbrev-mode)
+
+(with-eval-after-load 'latex-mode
+  ;; 1. INLINE EXPANSIONS (Abbrevs)
+  ;; Type the shortcut followed by Space or Punctuation to expand
+  (define-abbrev latex-mode-abbrev-table "tbf" "\\textbf{}")
+  (define-abbrev latex-mode-abbrev-table "tit" "\\textit{}")
+  (define-abbrev latex-mode-abbrev-table "mit" "\\item")
+
+  ;; 2. STRUCTURAL TEMPLATES (Skeletons)
+  
+  ;; Your Custom Main Document Template
+  (define-skeleton latex-main-template
+    "Inserts Krishna's custom xelatex template."
+    nil
+    "% Created " (format-time-string "%Y-%m-%d %a %H:%M") "\n"
+    "% Intended LaTeX compiler: xelatex\n"
+    "\\documentclass[12pt]{article}\n"
+    "\\usepackage[utf8]{inputenc}\n"
+    "\\usepackage[T1]{fontenc}\n"
+    "\\usepackage{graphicx}\n"
+    "\\usepackage{longtable}\n"
+    "\\usepackage{wrapfig}\n"
+    "\\usepackage[margin=1in]{geometry}\n"
+    "\\usepackage{rotating}\n"
+    "\\usepackage{setspace}\n"
+    "\\onehalfspacing\n"
+    "\\usepackage[normalem]{ulem}\n"
+    "\\usepackage{amsmath}\n"
+    "\\usepackage{amssymb}\n"
+    "\\usepackage{capt-of}\n"
+    "\\setcounter{secnumdepth}{4} % Tells LaTeX to number down to paragraphs\n"
+    "\\setcounter{tocdepth}{4}  \n"
+    "\\usepackage{hyperref}\n"
+    "\\usepackage[style=oscola, backend=biber]{biblatex}\n"
+    "\\addbibresource{/Users/krishnajani/Documents/Libib.bib}\n"
+    "\\usepackage{csquotes}\n"
+    "\\usepackage[british]{babel}\n"
+    "\\setlength{\\parskip}{0.5\\baselineskip}\n"
+
+    "\\author{Krishna Jani}\n"
+    "\\date{\\today}\n"
+    "\\title{" (skeleton-read "Document Title: ") "}\n\n"
+    "\\begin{document}\n"
+    "\\maketitle\n\n"
+    _ "\n\n"
+    "\\end{document}")
+  
+  ;; Enumerate List Template
+  (define-skeleton latex-enumerate-template
+    "Inserts an enumerate environment with a starting item."
+    nil
+    "\\begin{enumerate}\n"
+    "  \\item " _ "\n"
+    "\\end{enumerate}")
+
+  ;; 3. KEYBINDINGS FOR SKELETONS
+  ;; Maps the templates to easy keyboard shortcuts in LaTeX mode
+  (define-key latex-mode-map (kbd "C-c t") 'latex-main-template)
+  (define-key latex-mode-map (kbd "C-c n") 'latex-enumerate-template))
+
+(vertico-mode 1)
 
 
+(if after-init-time
+    (edit-server-start)
+  (add-hook 'after-init-hook
+            #'(lambda() (edit-server-start))))
+(setq edit-server-new-frame-alist
+      '((name . "Edit with Emacs FRAME")
+        (top . 200)
+        (left . 200)
+        (width . 80)
+        (height . 25)
+        (minibuffer . t)
+        (menu-bar-lines . t)
+        (window-system . x)))
 
-(set-register ?s (cons 'file "~/Documents/scratchpad.org"))
-
-
-
-(use-package yasnippet
-  :ensure t
-  :config
-  (yas-global-mode 1)
-  (setq yas-snippet-dirs
-	'("~/.emacs.d/snippets")))
-
-(defun kj/sync-mail () (interactive)
-       (async-shell-command "sh ~/.local/bin/mm"))
-
-(defun kj/insert-date () (interactive)
-       (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
-
-
-;(set-background-color "black")
-;(set-foreground-color "white")
-
-(use-package acme-theme
-  :ensure t
-  :config
-  (load-theme 'acme 1))
-
-    
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(citar-bibliography
-   '("/Users/krishnajani/Library/Mobile Documents/com~apple~CloudDocs/Documents/Library.bib"))
- '(custom-safe-themes
-   '("850af01ee34a0d0a0b19ff7ef239bde103cbb87f31ce54d4cd6b17de45e632bb"
-     default))
- '(evil-want-keybinding nil)
- '(inverse-video t)
- '(line-spacing 0.3)
- '(make-backup-files nil)
- '(org-agenda-files
-   '("~/Library/Mobile Documents/com~apple~CloudDocs/Org/scratchpad.org"))
- '(org-cite-global-bibliography
-   '("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/Library.bib"))
- '(org-file-apps
-   '((auto-mode . emacs) (directory . emacs) ("\\.mm\\'" . default)
-     ("\\.x?html?\\'" . default) ("\\.pdf\\'" . system)))
- '(org-image-max-width 500)
- '(org-list-allow-alphabetical t)
- '(org-roam-capture-templates
-   '(("d" "default" plain "%?" :target
-      (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-		 "#+title: ${title} #+cite_export: csl /home/kjani/Zotero/styles/oscola.csl\12")
-      :unnarrowed t)))
- '(package-selected-packages
-   '(acme-theme calfw-org citar consult magit marginalia obsidian
-		olivetti org-modern org-roam vertico yasnippet))
- '(select-enable-clipboard t))
-    
-
+ '(citar-bibliography '("/Users/krishnajani/Documents/Libib.bib"))
+ '(package-selected-packages '(auctex citar edit-server magit vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
